0cc29a1d2fe33e17fe9198ba27bca88aaab98898
debmittarv <debabrata.basak@mittarv.com>
2023-06-23 03:55:21 +0530

    initial commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 require("./database/connection")();
 const corsOptions = {
   // origin: "http://localhost:3000",
-  origin:"https://skids-frontend.netlify.app/",
+  origin:"https://skids-frontend.netlify.app",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
